2dd7d5332615f3d5e51f34ba55a46680afdc4e43
abeeraleissa <52518879+abeeraleissa@users.noreply.github.com>
2019-08-23 00:12:50 -0700

    Add files via upload

--- collatz.py
+++ collatz.py
@@ -1,12 +1,13 @@
- def collatz(num):
-    while num != 1:    
-        if num %2 == 0:
-            num = num //2
+def collatz(num):
+    while num != 1:
+        if num % 2 == 0:
+            num = num // 2
             print(num)
         else:
-            num = (3*num)+1
+            num = (3 * num) + 1
             print(num)
-            
+
+
 num = int(input('Enter integer number: '))
 print(num)
 collatz(num)
